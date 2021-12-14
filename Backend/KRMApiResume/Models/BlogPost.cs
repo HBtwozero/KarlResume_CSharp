67ec1ee0ba15a95308c912396d951d0da23eacd2
HBtwoZero <53544787+HBtwozero@users.noreply.github.com>
2021-12-14 10:31:43 -0500

    added api with .net core and also drafted api endpoint for blog controller

---
+++ Backend/KRMApiResume/Models/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KRMApiResume.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Body { get; set; }
+        public Author Author { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
+
+//https://medium.com/@kristaps.strals/create-your-own-blog-net-core-f3e6fefafb06
